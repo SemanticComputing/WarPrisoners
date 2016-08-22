9836f9d750b0230909fb3fd48e84d7017759c052
Mikko Koho <mikko.koho@iki.fi>
2016-08-22 15:45:06 +0300

    Working conversion and schema writing

--- csv_to_rdf.py
+++ csv_to_rdf.py
@@ -6,7 +6,6 @@ Convert Prisoners of War from CSV to RDF.
 
 import argparse
 import logging
-from math import nan
 import os
 
 import re
@@ -17,15 +16,17 @@ import pandas as pd
 
 #################################
 
-NAMESPACE = 'http://ldf.fi/warsa/prisoners/'
+DATA_NAMESPACE = 'http://ldf.fi/warsa/prisoners/'
+SCHEMA_NAMESPACE = 'http://ldf.fi/schema/warsa/prisoners/'
+
 PROPERTY_MAPPING = {
-    'suku- ja etunimet': {'uri': URIRef(NAMESPACE + 'nimi'), 'name': 'Nimi'},
-    'syntymäaika': {'uri': URIRef(NAMESPACE + 'syntymaaika'), 'name': 'Syntymäaika'},
-    'syntymäpaikka': {'uri': URIRef(NAMESPACE + 'syntymapaikka'), 'name': 'Syntymäpaikka'},
-    'kotipaikka': {'uri': URIRef(NAMESPACE + 'kotipaikka'), 'name': 'Kotipaikka'},
-    'asuinpaikka': {'uri': URIRef(NAMESPACE + 'asuinpaikka'), 'name': 'Asuinpaikka'},
-    'ammatti': {'uri': URIRef(NAMESPACE + 'ammatti'), 'name': 'Ammatti'},
-    'siviilisääty': {'uri': URIRef(NAMESPACE + 'siviilisaaty'), 'name': 'Siviilisääty'},
+    'suku- ja etunimet': {'uri': URIRef(SCHEMA_NAMESPACE + 'nimi'), 'name': 'Nimi'},
+    'syntymäaika': {'uri': URIRef(SCHEMA_NAMESPACE + 'syntymaaika'), 'name': 'Syntymäaika'},
+    'syntymäpaikka': {'uri': URIRef(SCHEMA_NAMESPACE + 'syntymapaikka'), 'name': 'Syntymäpaikka'},
+    'kotipaikka': {'uri': URIRef(SCHEMA_NAMESPACE + 'kotipaikka'), 'name': 'Kotipaikka'},
+    'asuinpaikka': {'uri': URIRef(SCHEMA_NAMESPACE + 'asuinpaikka'), 'name': 'Asuinpaikka'},
+    'ammatti': {'uri': URIRef(SCHEMA_NAMESPACE + 'ammatti'), 'name': 'Ammatti'},
+    'siviilisääty': {'uri': URIRef(SCHEMA_NAMESPACE + 'siviilisaaty'), 'name': 'Siviilisääty'},
 }
 
 INPUT_FILE_DIRECTORY = 'data/'
@@ -46,14 +47,17 @@ column_headers = list(table)
 for index in [0]:
     for column in range(0, len(column_headers)):
         column_name = column_headers[column]
-        prisoner_uri = URIRef(NAMESPACE + 'prisoner_' + str(index))
+        prisoner_uri = URIRef(DATA_NAMESPACE + 'prisoner_' + str(index))
 
         if column_name in PROPERTY_MAPPING:
             value = table.ix[index][column] if pd.notnull(table.ix[index][column]) else None
 
             if value:
-                log.warning('Object type: {type}, value: {value}'.format(type=type(value), value=value))
-
                 data.add((prisoner_uri, PROPERTY_MAPPING[column_name]['uri'], Literal(value)))
 
+schema = Graph()
+for prop in PROPERTY_MAPPING.values():
+    schema.add((prop['uri'], RDFS.label, Literal(prop['name'])))
+
 data.serialize(format="turtle", destination=OUTPUT_FILE_DIRECTORY + "prisoners.ttl")
+schema.serialize(format="turtle", destination=OUTPUT_FILE_DIRECTORY + "schema.ttl")
