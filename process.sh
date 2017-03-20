b84eb118741ce1787fa3f0f57460bc8a1a3c725b
Mikko Koho <mikko.koho@iki.fi>
2017-03-20 13:38:17 +0200

    Add some TODOs for linking

--- process.sh
+++ process.sh
@@ -2,3 +2,10 @@
 python csv_to_rdf.py data/prisoners.csv data/new/ PRISONERS
 
 python csv_to_rdf.py data/camps.csv data/new/ CAMPS
+
+# TODO: Link camps
+# TODO: Link persons using Arpa-linker and jellyfish
+# TODO: Link military ranks using Arpa-linker
+# TODO: Link military units using Arpa-linker
+# TODO: Link places using Arpa-linker
+
