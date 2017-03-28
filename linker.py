8a663a1373c5b7ce1cad475290ec8944ac1cc0ff
Mikko Koho <mikko.koho@aalto.fi>
2017-03-28 13:56:41 +0300

    Add link function to do actual linking

--- linker.py
+++ linker.py
@@ -6,6 +6,8 @@ import argparse
 import logging
 import re
 
+import itertools
+
 from arpa_linker.arpa import ArpaMimic
 from rdflib import Graph
 from rdflib import URIRef
@@ -14,7 +16,49 @@ from rdflib.util import guess_format
 from namespaces import *
 
 
-def link_ranks(graph, endpoint, prop=SCHEMA_NS.rank):
+def link(graph, endpoint, prop, query, preprocess=lambda x: x, mapping=None, validate=None):
+    """
+    Link entities based on parameters
+    
+    :return: 
+    """
+    if not mapping:
+        mapping = {}
+
+    prop_str = str(prop).split('/')[-1]  # Used for logging
+
+    arpa = ArpaMimic(query, url=endpoint, retries=3, wait_between_tries=3)
+
+    for (prisoner, value_literal) in list(graph[:prop:]):
+        value = str(value_literal).strip()
+        value = preprocess(value)
+        value = mapping[value] if value in mapping else value
+
+        if value != str(value_literal):
+            logger.info('Changed %s %s into %s for linking' % (prop_str, value_literal, value))
+
+        if value:
+            res = arpa.query(value)
+            if res:
+                res = res[0]['id']
+                logger.debug('Found a matching {ps} for {val}: {res}'.format(ps=prop_str, val=value, res=res))
+
+                if validate and not validate(res):
+                    logger.info('Match {res} failed validation for {val}, skipping it')
+                    continue
+
+                # Update property to found value
+                graph.remove((prisoner, prop, value_literal))
+                graph.add((prisoner, prop, URIRef(res)))
+
+                # TODO: Update reifications
+            else:
+                logger.warning('No match found for %s: %s' % (prop_str, value))
+
+    return graph
+
+
+def link_ranks(graph, endpoint):
     """
     Link military ranks in graph.
 
@@ -23,7 +67,9 @@ def link_ranks(graph, endpoint, prop=SCHEMA_NS.rank):
     :param prop: Property used to give military rank (used for both source and target) 
     :return: RDFLib Graph with updated links
     """
-    MAPPING = {'kaart': 'stm',
+    preprocess = lambda literal: re.sub(r'[/\-]', ' ', str(literal)).strip()
+
+    mapping = {'kaart': 'stm',
                'aliluutn': 'aliluutnantti'}
 
     query = "PREFIX text: <http://jena.apache.org/text#> " +\
@@ -31,27 +77,74 @@ def link_ranks(graph, endpoint, prop=SCHEMA_NS.rank):
             "?id text:query \"<VALUES>\" . " +\
             "}"
 
-    arpa = ArpaMimic(query.replace("\n", ""), url=endpoint, retries=3, wait_between_tries=3)
+    return link(graph, endpoint, SCHEMA_NS.rank, query, preprocess=preprocess, mapping=mapping)
 
-    for (prisoner, rank_literal) in list(graph[:prop:]):
-        rank = re.sub(r'[/\-]', ' ', str(rank_literal)).strip()
-        if rank != str(rank_literal):
-            logger.info('Changed rank %s into %s for linking.' % (rank_literal, rank))
-        if rank:
-            res = arpa.query(MAPPING[rank] if rank in MAPPING else rank)
-            if res:
-                res = res[0]['id']
-                logger.debug('Found a matching rank for {rank}: {res}'.format(rank=rank, res=res))
 
-                # Update property to found value
-                graph.remove((prisoner, prop, rank_literal))
-                graph.add((prisoner, prop, URIRef(res)))
+def _create_unit_abbreviations(text, *args):
+    """
+    Preprocess military unit abbreviation strings for all possible combinations
 
-                # TODO: Update reifications
-            else:
-                logger.warning('No match found for rank %s' % rank)
+    :param text: Military unit abbreviation
+    :return: List containing all possible abbrevations
 
-    return graph
+    >>> _create_unit_abbreviations('3./JR 1')
+    '3./JR 1 # 3./JR. 1. # 3./JR.1. # 3./JR1 # 3/JR 1 # 3/JR. 1. # 3/JR.1. # 3/JR1 # JR 1 # JR. 1. # JR.1. # JR1'
+    >>> _create_unit_abbreviations('27.LK')
+    '27 LK # 27. LK. # 27.LK # 27.LK. # 27LK # 27 LK # 27. LK. # 27.LK # 27.LK. # 27LK'
+    >>> _create_unit_abbreviations('P/L Ilmarinen')
+    'P./L Ilmarinen # P./L. Ilmarinen. # P./L.Ilmarinen. # P./LIlmarinen # P/L Ilmarinen # P/L. Ilmarinen. # P/L.Ilmarinen. # P/LIlmarinen # L Ilmarinen # L. Ilmarinen. # L.Ilmarinen. # LIlmarinen # P # P.'
+    """
+
+    # TODO: Copy-pasted from casualties linking, should use this commonly if no customization needed
+
+    def _split(part):
+        return [a for a, b in re.findall(r'(\w+?)(\b|(?<=[a-zäö])(?=[A-ZÄÖ]))', part)]
+        # return [p.strip() for p in part.split('.')]
+
+    def _variations(part):
+        inner_parts = _split(part) + ['']
+        variations = []
+        variations += ['.'.join(inner_parts)]
+        variations += ['. '.join(inner_parts)]
+        variations += [' '.join(inner_parts)]
+        variations += [''.join(inner_parts)]
+        return variations
+
+    variation_lists = [_variations(part) + [part] for part in text.split('/')]
+
+    combined_variations = sorted(set(['/'.join(combined).strip().replace(' /', '/')
+                                      for combined in sorted(set(itertools.product(*variation_lists)))]))
+
+    variationset = set(variation.strip() for var_list in variation_lists for variation in var_list
+                       if not re.search(r'^[0-9](\.)?$', variation.strip()))
+
+    return ' # '.join(combined_variations) + ' # ' + ' # '.join(sorted(variationset))
+
+
+def link_units(graph, endpoint):
+    """
+    Link military units in graph.
+
+    :param graph: Data in RDFLib Graph object
+    :param endpoint: Endpoint to query
+    :param prop: Property used to give military unit (used for both source and target)
+    :return: RDFLib Graph with updated links
+    """
+    preprocess = lambda literal: _create_unit_abbreviations(str(literal).strip())
+
+    mapping = {}
+
+    query = "PREFIX text: <http://jena.apache.org/text#> " \
+            "PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/> " \
+            "SELECT * { ?id a <http://ldf.fi/warsa/actors/actor_types/MilitaryUnit> . " \
+            "?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P82a_begin_of_the_begin " \
+            "    ?conflict_start . " \
+            "?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P82b_end_of_the_end " \
+            "    ?conflict_end . " \
+            "?id text:query \"<VALUES>\" . " \
+            "}"
+
+    return link(graph, endpoint, SCHEMA_NS.unit, query, preprocess=preprocess, mapping=mapping, validate=print)
 
 
 if __name__ == '__main__':
@@ -79,4 +172,9 @@ if __name__ == '__main__':
     if args.task == 'ranks':
         logger.info('Linking ranks')
         link_ranks(input_graph, args.endpoint).serialize(args.output, format=guess_format(args.output))
+
+    elif args.task == 'units':
+        logger.info('Linking units')
+        link_units(input_graph, args.endpoint).serialize(args.output, format=guess_format(args.output))
+
 
