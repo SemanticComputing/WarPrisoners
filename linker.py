ee961ba531fec2d47130e48571ed15cbcf72ac39
Mikko Koho <mikko.koho@aalto.fi>
2017-04-27 11:41:18 +0300

    Link units with ARPA

--- linker.py
+++ linker.py
@@ -14,7 +14,7 @@ import copy
 from fuzzywuzzy import fuzz
 from rdflib.compare import graph_diff
 
-from arpa_linker.arpa import ArpaMimic, arpafy
+from arpa_linker.arpa import ArpaMimic, arpafy, Arpa
 from rdflib import Graph
 from rdflib import URIRef
 from rdflib.util import guess_format
@@ -44,7 +44,7 @@ def link(graph, arpa, prop, preprocess=_preprocess, validator=None):
     for (prisoner, value_literal) in list(graph[:prop:]):
         value = preprocess(value_literal, prisoner, graph)
 
-        logger.debug('Finding links for %s (originally %s)' % (value, value_literal))
+        log.debug('Finding links for %s (originally %s)' % (value, value_literal))
 
         if value:
             arpa_result = arpa.query(value)
@@ -54,12 +54,12 @@ def link(graph, arpa, prop, preprocess=_preprocess, validator=None):
                 if validator:
                     res = validator.validate(arpa_result, value_literal, prisoner)
                     if not res:
-                        logger.info('Match {res} failed validation for {val}, skipping it'.
-                                    format(res=res, val=value_literal))
+                        log.info('Match {res} failed validation for {val}, skipping it'.
+                                 format(res=res, val=value_literal))
                         continue
 
-                logger.info('Accepted a match for property {ps} with original value {val} : {res}'.
-                             format(ps=prop_str, val=value_literal, res=res))
+                log.info('Accepted a match for property {ps} with original value {val} : {res}'.
+                         format(ps=prop_str, val=value_literal, res=res))
 
                 # Update property to found value
                 graph.remove((prisoner, prop, value_literal))
@@ -67,7 +67,7 @@ def link(graph, arpa, prop, preprocess=_preprocess, validator=None):
 
                 # TODO: Update reifications
             else:
-                logger.warning('No match found for %s: %s' % (prop_str, value))
+                log.warning('No match found for %s: %s' % (prop_str, value))
 
     return graph
 
@@ -111,6 +111,8 @@ def _create_unit_abbreviations(text, *args):
     '27 LK # 27. LK. # 27.LK # 27.LK. # 27LK # 27 LK # 27. LK. # 27.LK # 27.LK. # 27LK'
     >>> _create_unit_abbreviations('P/L Ilmarinen')
     'P./L Ilmarinen # P./L. Ilmarinen. # P./L.Ilmarinen. # P./LIlmarinen # P/L Ilmarinen # P/L. Ilmarinen. # P/L.Ilmarinen. # P/LIlmarinen # L Ilmarinen # L. Ilmarinen. # L.Ilmarinen. # LIlmarinen # P # P.'
+    >>> '27. Pion.K.' in _create_unit_abbreviations('27.Pion.K.')
+    True
     """
 
     # TODO: Copy-pasted from casualties linking, should use this commonly if no customization needed
@@ -121,12 +123,17 @@ def _create_unit_abbreviations(text, *args):
 
     def _variations(part):
         inner_parts = _split(part) + ['']
-        variations = []
-        variations += ['.'.join(inner_parts)]
-        variations += ['. '.join(inner_parts)]
+
+        spacecombos = list(itertools.product(['.', '. '], repeat=len(inner_parts) - 1))
+        combined = [tuple(zip(inner_parts, spacecombo)) for spacecombo in spacecombos]
+        combined_strings = [''.join(cc[0] + cc[1] for cc in inner).strip() for inner in combined]
+
+        variations = list(set(combined_strings))
+        # variations += ['.'.join(inner_parts)]
+        # variations += ['. '.join(inner_parts)]
         variations += [' '.join(inner_parts)]
         variations += [''.join(inner_parts)]
-        return variations
+        return sorted(variations)
 
     variation_lists = [_variations(part) + [part] for part in text.split('/')]
 
@@ -162,7 +169,7 @@ class UnitValidator:
                 unit_begin = unit['properties']['begin'][0].split('"')[1]
                 unit_end = unit['properties']['end'][0].split('"')[1]
             except (TypeError, KeyError):
-                logger.warning('Unable to read data for validation for {uri} , skipping result...'.format(uri=unit))
+                log.warning('Unable to read data for validation for {uri} , skipping result...'.format(uri=unit))
                 continue
 
             prisoner_caught = str(self.graph.value(prisoner, SCHEMA_NS.time_captured))
@@ -203,23 +210,59 @@ def link_units(graph, endpoint):
     def preprocess(literal, prisoner, subgraph):
         return _create_unit_abbreviations(str(literal).strip())
 
-    query = "PREFIX text: <http://jena.apache.org/text#>" \
-            "PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>" \
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" \
-            "SELECT ?id ?label (min(?conflict_start) as ?begin) (max(?conflict_end) as ?end) {" \
-            "  ?id a <http://ldf.fi/warsa/actors/actor_types/MilitaryUnit> ." \
-            "  OPTIONAL {?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P4_has_time-span/crm:P82a_begin_of_the_begin" \
-            "        ?conflict_start ." \
-            "    ?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P4_has_time-span/crm:P82b_end_of_the_end" \
-            "        ?conflict_end . }" \
-            "  ?id rdfs:label ?label ." \
-            "  ?id text:query \"<VALUES>\" ." \
-            "} GROUP BY ?id ?label"
-    # TODO: skos:prefLabel, skos:altLabel
+    def split_by_war(pgraph: Graph):
+        for person in pgraph[:RDF.type:SCHEMA_NS.PrisonerOfWar]:
+            time_captured = pgraph.value(person, SCHEMA_NS.time_captured)  # Assume duplicates are from same war
+            stime = str(time_captured).lower()
+            unit = pgraph.value(person, SCHEMA_NS.unit)  # Expecting only one unit per person
 
-    arpa = ArpaMimic(query, url=endpoint, retries=3, wait_between_tries=3)
+            if not unit:
+                continue
 
-    return link(graph, arpa, SCHEMA_NS.unit, preprocess=preprocess, validator=UnitValidator(graph))
+            pgraph.remove((person, SCHEMA_NS.unit, None))
+
+            if stime <= '1941-06-25' or stime == 'talvisota':
+                pgraph.add((person, SCHEMA_NS.unit_winter, unit))
+            else:
+                pgraph.add((person, SCHEMA_NS.unit_continuation, unit))
+
+        return pgraph
+
+    # query = "PREFIX text: <http://jena.apache.org/text#>" \
+    #         "PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>" \
+    #         "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" \
+    #         "SELECT ?id ?label (min(?conflict_start) as ?begin) (max(?conflict_end) as ?end) {" \
+    #         "  ?id a <http://ldf.fi/warsa/actors/actor_types/MilitaryUnit> ." \
+    #         "  OPTIONAL {?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P4_has_time-span/crm:P82a_begin_of_the_begin" \
+    #         "        ?conflict_start ." \
+    #         "    ?id <http://ldf.fi/warsa/actors/hasConflict>/crm:P4_has_time-span/crm:P82b_end_of_the_end" \
+    #         "        ?conflict_end . }" \
+    #         "  ?id rdfs:label ?label ." \
+    #         "  ?id text:query \"<VALUES>\" ." \
+    #         "} GROUP BY ?id ?label"
+    # # TODO: skos:prefLabel, skos:altLabel
+    #
+    # arpa = ArpaMimic(query, url=endpoint, retries=3, wait_between_tries=3)
+
+    graph = split_by_war(graph)
+    log.debug('Found %s winter war units to link' % len(list(graph[:SCHEMA_NS.unit_winter:])))
+    log.debug('Found %s continuation war units to link' % len(list(graph[:SCHEMA_NS.unit_continuation:])))
+
+    arpa = Arpa('http://demo.seco.tkk.fi/arpa/warsa_actor_units_winterwar')
+    res = arpafy(graph, SCHEMA_NS.unit_linked, arpa, SCHEMA_NS.unit_winter,
+                    preprocessor=_create_unit_abbreviations, progress=True)
+
+    print(res)
+
+    arpa = Arpa('http://demo.seco.tkk.fi/arpa/warsa_actor_units_continuationwar')
+    res = arpafy(graph, SCHEMA_NS.unit_linked, arpa, SCHEMA_NS.unit_continuation,
+                    preprocessor=_create_unit_abbreviations, progress=True)
+
+    print(res)
+
+    return graph
+
+    # return link(graph, arpa, SCHEMA_NS.unit, preprocess=preprocess, validator=UnitValidator(graph))
 
 
 if __name__ == '__main__':
@@ -239,17 +282,17 @@ if __name__ == '__main__':
                         level=getattr(logging, args.loglevel),
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    logger = logging.getLogger(__name__)
+    log = logging.getLogger(__name__)
 
     input_graph = Graph()
     input_graph.parse(args.input, format=guess_format(args.input))
 
     if args.task == 'ranks':
-        logger.info('Linking ranks')
+        log.info('Linking ranks')
         link_ranks(input_graph, args.endpoint).serialize(args.output, format=guess_format(args.output))
 
     elif args.task == 'units':
-        logger.info('Linking units')
+        log.info('Linking units')
         link_units(input_graph, args.endpoint).serialize(args.output, format=guess_format(args.output))
 
 
--- process.sh
+++ process.sh
@@ -2,9 +2,9 @@
 python csv_to_rdf.py data/prisoners.csv data/new/ PRISONERS
 python csv_to_rdf.py data/camps.csv data/new/ CAMPS
 
+mv data/new/prisoners.ttl data/new/prisoners_unlinked.ttl
 python linker.py ranks data/new/prisoners.ttl data/new/prisoners_linked.ttl --endpoint "http://localhost:3030/warsa/sparql"
 
-# TODO: Link military units using Arpa-linker
 # TODO: Link persons using Arpa-linker and jellyfish
 
 # TODO: Link camps
