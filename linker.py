7e6be06e0c65bb55967eb0dd19d0e2eefcacd28f
Erkki Heino <erkki.heino@aalto.fi>
2017-09-08 18:17:36 +0300

    Improved person linking

--- linker.py
+++ linker.py
@@ -5,16 +5,17 @@
 import argparse
 import logging
 import re
-from pprint import pprint
+import time
+from datetime import datetime
 
-from SPARQLWrapper import SPARQLWrapper
+from jellyfish import jaro_winkler
 from fuzzywuzzy import fuzz
-from rdflib import Graph, Literal
+from rdflib import Graph
 from rdflib import URIRef
 from rdflib.util import guess_format
 
-from arpa_linker.arpa import ArpaMimic, Arpa, combine_values, process_graph
-from namespaces import SCHEMA_NS, RDF, SKOS, FOAF
+from arpa_linker.arpa import ArpaMimic, process_graph
+from namespaces import SCHEMA_NS, SKOS, FOAF, CIDOC, BIOC
 
 
 # TODO: Write some tests using responses
@@ -32,7 +33,6 @@ def link(graph, arpa, source_prop, target_graph, target_prop, preprocess=_prepro
     :return: target_graph with found links
     """
     prop_str = str(source_prop).split('/')[-1]  # Used for logging
-    new_graph = Graph()
 
     for (prisoner, value_literal) in list(graph[:source_prop:]):
         value = preprocess(value_literal, prisoner, graph)
@@ -97,59 +97,97 @@ def link_ranks(graph, endpoint):
 
 class PersonValidator:
     def __init__(self, graph, birthdate_prop, deathdate_prop, source_rank_prop,
-                 source_firstname_prop, source_lastname_prop):
+                 source_firstname_prop, source_lastname_prop, disappearance_place_prop,
+                 disappearance_date_prop, birth_place_prop, unit_prop, occupation_prop):
         self.graph = graph
         self.birthdate_prop = birthdate_prop
         self.deathdate_prop = deathdate_prop
         self.source_rank_prop = source_rank_prop
         self.source_firstname_prop = source_firstname_prop
         self.source_lastname_prop = source_lastname_prop
+        self.birth_place_prop = birth_place_prop
+        self.disappearance_place_prop = disappearance_place_prop
+        self.unit_prop = unit_prop
+        self.occupation_prop = occupation_prop
+        self.disappearance_date_prop = disappearance_date_prop
 
     def validate(self, results, text, s):
         if not results:
             return results
 
         rank = self.graph.value(s, self.source_rank_prop)
+        unit = self.graph.value(s, self.unit_prop)
         firstnames = str(self.graph.value(s, self.source_firstname_prop)).replace('/', ' ').lower().split()
         lastname = str(self.graph.value(s, self.source_lastname_prop)).replace('/', ' ').lower()
+        birth_place = (self.graph.value(s, self.birth_place_prop) or '').lower()
+        disappearance_place = (self.graph.value(s, self.disappearance_place_prop) or '').lower()
+        disappearance_date = (self.graph.value(s, self.disappearance_date_prop) or '').lower()
+        occupation = (self.graph.value(s, self.occupation_prop) or '').lower()
+
+        bd = [str(b) for b in self.graph.objects(s, self.birthdate_prop)]
+        birthdates = set()
+        for b in bd:
+            if len(re.findall('x', b, re.I)) < 2:
+                birthdates.add(b)
+
+        dd = [str(d) for d in self.graph.objects(s, self.deathdate_prop)]
+        deathdates = set()
+        for d in dd:
+            if len(re.findall('x', d, re.I)) < 2:
+                deathdates.add(d)
+
+        disappearance_date = str(self.graph.value(s, self.disappearance_date_prop))
+        if len(re.findall('x', disappearance_date, re.I)) > 1:
+            disappearance_date = None
 
         filtered = []
-        _FUZZY_LASTNAME_MATCH_LIMIT = 50
-        _FUZZY_FIRSTNAME_MATCH_LIMIT = 60
+        _FUZZY_LASTNAME_MATCH_LIMIT = 0.5
+        _FUZZY_FIRSTNAME_MATCH_LIMIT = 0.5
+        DATE_FORMAT = '%Y-%m-%d'
 
         for person in results:
             score = 0
             res_id = None
             try:
                 res_id = person['properties'].get('id')[0].replace('"', '')
-                res_ranks = [r.replace('"', '').lower() for r in person['properties'].get('rank_id', [''])]
+                res_ranks = [URIRef(re.sub(r'[<>]', '', r)) for r in person['properties'].get('rank_id', ['']) if r]
 
                 res_lastname = person['properties'].get('sukunimi')[0].replace('"', '').lower()
                 res_firstnames = person['properties'].get('etunimet')[0].split('^')[0].replace('"', '').lower()
                 res_firstnames = res_firstnames.split()
 
-                res_birthdates = (min(person['properties'].get('birth_start', [''])).split('^')[0].replace('"', ''),
-                                  max(person['properties'].get('birth_end', [''])).split('^')[0].replace('"', ''))
-                res_deathdates = (min(person['properties'].get('death_start', [''])).split('^')[0].replace('"', ''),
-                                  max(person['properties'].get('death_end', [''])).split('^')[0].replace('"', ''))
+                res_birth_place = person['properties'].get('birth_place', [''])[0].replace('"', '').lower()
+                res_disappearance_place = person['properties'].get('disappearance_place', [''])[0].replace('"', '').lower()
+                res_disappearance_date = person['properties'].get('disappearance_date', [''])[0].replace('"', '').lower()
+                res_occupation = person['properties'].get('occupation', [''])[0].replace('"', '').lower()
+                res_unit = person['properties'].get('unit', [''])[0]
+                res_unit = URIRef(re.sub(r'[<>]', '', res_unit)) if res_unit else res_unit
+
+                res_bd = (min(person['properties'].get('birth_start', [''])).split('^')[0].replace('"', ''),
+                            max(person['properties'].get('birth_end', [''])).split('^')[0].replace('"', ''))
+                res_birthdates = set(filter(bool, res_bd))
+                res_dd = (min(person['properties'].get('death_start', [''])).split('^')[0].replace('"', ''),
+                            max(person['properties'].get('death_end', [''])).split('^')[0].replace('"', ''))
+                res_deathdates = set(filter(bool, res_dd))
 
             except TypeError:
                 log.info('Unable to read data for validation for {uri} , skipping result...'.format(uri=res_id))
                 continue
 
             log.debug('Potential match for person {p1text} <{p1}> : {p2text} {p2}'.
-                      format(p1text=' '.join([rank] + firstnames + [lastname]),
+                      format(p1text=' '.join([rank or ''] + firstnames + [lastname]),
                              p1=s,
                              p2text=' '.join(res_ranks + res_firstnames + [res_lastname]),
                              p2=res_id))
 
-            fuzzy_lastname_match = fuzz.token_set_ratio(lastname, res_lastname, force_ascii=False)
+            fuzzy_lastname_match = jaro_winkler(lastname, res_lastname)
 
             if fuzzy_lastname_match >= _FUZZY_LASTNAME_MATCH_LIMIT:
                 log.debug('Fuzzy last name match for {f1} and {f2}: {fuzzy}'
                           .format(f1=lastname, f2=res_lastname, fuzzy=fuzzy_lastname_match))
-                score += int((fuzzy_lastname_match - _FUZZY_LASTNAME_MATCH_LIMIT) /
-                             (100 - _FUZZY_LASTNAME_MATCH_LIMIT) * 100)
+                score += (fuzzy_lastname_match - _FUZZY_LASTNAME_MATCH_LIMIT) / (1 - _FUZZY_LASTNAME_MATCH_LIMIT) * 100
+
+            log.info('Lastname: {b} <-> {rb}: -> {s}'.format(b=lastname, rb=res_lastname, s=score))
 
             if rank and res_ranks and rank != 'tuntematon':
                 if rank in res_ranks:
@@ -161,74 +199,97 @@ class PersonValidator:
                 else:
                     score -= 25
 
-            birthdate = str(self.graph.value(s, self.birthdate_prop))
-            deathdate = str(self.graph.value(s, self.deathdate_prop))
+            log.info('Rank: {b} <-> {rb}: -> {s}'.format(b=rank, rb=res_ranks, s=score))
 
-            if res_birthdates[0] and birthdate:
-                if res_birthdates[0] <= birthdate:
-                    if res_birthdates[0] == birthdate:
-                        score += 50
+            if res_birthdates and birthdates:
+                if res_birthdates.issubset(birthdates):
+                    score += 100
+                elif [d for d in birthdates if res_bd[0] <= d <= res_bd[1]]:
+                    score += 50
                 else:
                     score -= 25
 
-            if res_birthdates[1] and birthdate:
-                if birthdate <= res_birthdates[1]:
-                    if res_birthdates[1] == birthdate:
-                        score += 50
-                else:
+                # If both are single dates, allow one different character before penalizing more
+                if len(res_birthdates) == 1 and max([fuzz.partial_ratio(next(iter(res_birthdates)), d) for d in birthdates] or [0]) <= 80:
                     score -= 25
 
-            # If both are single dates, allow one different character before penalizing
-            if res_birthdates[0] and res_birthdates[0] == res_birthdates[1] and \
-               fuzz.partial_ratio(res_birthdates[0], birthdate) <= 80:
+            log.info('Birth: {b} <-> {rb}: -> {s}'.format(b=birthdates, rb=res_birthdates, s=score))
+
+            try:
+                [datetime.strptime(d, DATE_FORMAT) for d in deathdates]
+                has_parseable_death_date = True if deathdates else False
+            except ValueError:
+                has_parseable_death_date = False
+                log.warning('Could not parse death date: {date}'.format(date=deathdates))
+
+            ddates = set(deathdates)
+            if disappearance_date:
+                ddates.add(disappearance_date)
+            if res_deathdates and deathdates or res_disappearance_date and disappearance_date:
+                if deathdates == res_deathdates or disappearance_date == res_disappearance_date:
+                    score += 100
+                elif res_deathdates.issubset(ddates) or res_disappearance_date in ddates:
+                    score += 50
+                elif [d for d in ddates if res_dd[0] <= d <= res_dd[1]]:
+                    score += 50
+                elif deathdates and not (deathdates & res_deathdates):
+                    if has_parseable_death_date:
+                        score -= 25
+                elif disappearance_date and res_disappearance_date and disappearance_date != res_disappearance_date:
+                    if has_parseable_death_date:
+                        datetime.strptime(disappearance_date, DATE_FORMAT)
+                        score -= 25
+
+            # If both are single dates, allow one different character before penalizing more
+            if has_parseable_death_date and len(res_deathdates) == 1 and \
+                    max([fuzz.partial_ratio(next(iter(res_deathdates)), d) for d in deathdates]) <= 80:
                 score -= 25
 
-            if res_deathdates[0] and deathdate:
-                if res_deathdates[0] <= deathdate:
-                    if res_deathdates[0] == deathdate:
-                        score += 50
-                else:
-                    score -= 25
-
-            if res_deathdates[1] and deathdate:
-                if deathdate <= res_deathdates[1]:
-                    if deathdate == res_deathdates[1]:
-                        score += 50
-                else:
-                    score -= 25
-
-            # If both are single dates, allow one different character before penalizing
-            if res_deathdates[0] and res_deathdates[0] == res_deathdates[1] and \
-               fuzz.partial_ratio(res_deathdates[0], deathdate) <= 80:
-                score -= 25
+            log.info('Death: {b} <-> {rb}: -> {s}'.format(b=ddates, rb=res_deathdates, s=score))
 
             s_first1 = ' '.join(firstnames)
             s_first2 = ' '.join(res_firstnames)
-            fuzzy_firstname_match = max(fuzz.partial_ratio(s_first1, s_first2),
-                                        fuzz.token_sort_ratio(s_first1, s_first2, force_ascii=False),
-                                        fuzz.token_set_ratio(s_first1, s_first2, force_ascii=False))
+            fuzzy_firstname_match = fuzz.token_set_ratio(s_first1, s_first2) / 100
 
             if fuzzy_firstname_match >= _FUZZY_FIRSTNAME_MATCH_LIMIT:
-                log.debug('Fuzzy first name match for {f1} and {f2}: {fuzzy}'
-                          .format(f1=firstnames, f2=res_firstnames, fuzzy=fuzzy_firstname_match))
-                score += int((fuzzy_firstname_match - _FUZZY_FIRSTNAME_MATCH_LIMIT) /
-                             (100 - _FUZZY_FIRSTNAME_MATCH_LIMIT) * 100)
+                score += (fuzzy_firstname_match - _FUZZY_FIRSTNAME_MATCH_LIMIT) / (1 - _FUZZY_FIRSTNAME_MATCH_LIMIT) * 75
+                log.info('Fuzzy first name match for {f1} and {f2}: {fuzzy} -> {score}'
+                          .format(f1=firstnames, f2=res_firstnames, fuzzy=fuzzy_firstname_match, score=score))
             else:
-                log.debug('No fuzzy first name match for {f1} and {f2}: {fuzzy}'
+                log.info('No fuzzy first name match for {f1} and {f2}: {fuzzy}'
                           .format(f1=firstnames, f2=res_firstnames, fuzzy=fuzzy_firstname_match))
+
+            if birth_place and res_birth_place and birth_place == res_birth_place:
+                score += 30
+            log.info('BPlace: {b} <-> {rb} -> {s}'.format(b=birth_place, rb=res_birth_place, s=score))
+
+            if disappearance_place and res_disappearance_place and disappearance_place == res_disappearance_place:
+                score += 25
+            log.info('DPlace: {b} <-> {rb} -> {s}'.format(b=disappearance_place, rb=res_disappearance_place, s=score))
+
+            if unit and res_unit and unit == res_unit:
+                score += 15
+            log.info('Unit: {b} <-> {rb} -> {s}'.format(b=unit, rb=res_unit, s=score))
+
+            if occupation and res_occupation and occupation == res_occupation:
+                score += 20
+            log.info('Occupation: {b} <-> {rb} -> {s}'.format(b=occupation, rb=res_occupation, s=score))
 
             person['score'] = score
 
-            if score > 210:
-                filtered.append(person)
+            rank_name = re.sub(r'.+/(\w+?)$', r'\1', str(rank))
+            res_rank_name = ', '.join([re.sub(r'.+/(\w+?)$', r'\1', str(rank)) for rank in res_ranks])
 
-                log.info('Found matching Warsa person for {rank} {fn} {ln} {uri} : '
+            if score > 200:
+                filtered.append(person)
+                log.info('Found person for {rank} {fn} {ln} {uri} : '
                          '{res_rank} {res_fn} {res_ln} {res_uri} [score: {score}]'
-                         .format(rank=rank, fn=s_first1, ln=lastname, uri=s, res_rank=res_ranks, res_fn=s_first2,
+                         .format(rank=rank_name, fn=s_first1, ln=lastname, uri=s, res_rank=res_rank_name, res_fn=s_first2,
                                  res_ln=res_lastname, res_uri=res_id, score=score))
             else:
-                log.info('Skipping potential match because of too low score [{score}]: {p1}  <<-->>  {p2}'.
-                         format(p1=s, p2=res_id, score=score))
+                log.info('Skip low score [{score}]: {rank} {fn} {ln} {uri} <<-->> {res_rank} {res_fn} {res_ln} {res_uri}'
+                         .format(rank=rank_name, fn=s_first1, ln=lastname, uri=s, res_rank=res_rank_name, res_fn=s_first2,
+                                 res_ln=res_lastname, res_uri=res_id, score=score))
 
         if len(filtered) == 1:
             return filtered
@@ -259,62 +320,13 @@ def link_persons(graph, endpoint):
             return f.read()
 
     validator = PersonValidator(graph, SCHEMA_NS.birth_date, SCHEMA_NS.death_date,
-            SCHEMA_NS.warsa_rank, FOAF.givenName, FOAF.familyName)
+            SCHEMA_NS.warsa_rank, FOAF.givenName, FOAF.familyName,
+            SCHEMA_NS.place_captured_municipality, SCHEMA_NS.time_captured,
+            SCHEMA_NS.birth_place, SCHEMA_NS.warsa_unit, BIOC.has_occupation)
     arpa = ArpaMimic(get_query_template(), endpoint, retries=10, wait_between_tries=6)
-    new_graph = process_graph(graph, SCHEMA_NS.person, arpa, progress=True,
+    new_graph = process_graph(graph, CIDOC.P70_documents, arpa, progress=True,
                               validator=validator, new_graph=True, source_prop=SKOS.prefLabel)
     return new_graph['graph']
-
-
-class UnitValidator:
-    def __init__(self, graph):
-        """
-        :type graph: Graph
-        """
-        self.graph = graph
-
-    def validate(self, results, text, prisoner):
-        if not results:
-            return results
-
-        filtered = []
-
-        for unit in results:
-            unit_begin = None
-            unit_end = None
-            name = None
-            try:
-                name = unit['properties']['label'][0].split('"')[1]
-                unit_begin = unit['properties']['begin'][0].split('"')[1]
-                unit_end = unit['properties']['end'][0].split('"')[1]
-            except (TypeError, KeyError):
-                log.warning('Unable to read data for validation for {uri} , skipping result...'.format(uri=unit))
-                continue
-
-            prisoner_caught = str(self.graph.value(prisoner, SCHEMA_NS.time_captured))
-
-            print(text)
-            print(name)
-            if fuzz.token_set_ratio(text, name) < 80:
-                print(fuzz.token_set_ratio(text, name))
-                continue
-
-            if unit_begin and unit_begin > prisoner_caught:
-                if unit_end and prisoner_caught > unit_end:
-                    continue
-
-            filtered.append(unit)
-
-        if len(filtered) == 1:
-            return filtered
-        elif len(filtered) > 1:
-            pprint(len(results))
-            pprint(len(filtered))
-            return filtered
-
-        pprint(filtered)
-        quit()
-        return []
 
 
 if __name__ == '__main__':
@@ -329,7 +341,7 @@ if __name__ == '__main__':
 
     args = argparser.parse_args()
 
-    logging.basicConfig(filename='prisoners.log',
+    logging.basicConfig(filename='prisoners-{ts}.log'.format(ts=time.time()),
                         filemode='a',
                         level=getattr(logging, args.loglevel),
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
