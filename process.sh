a3c9056ed65969f8c0e5a9ec0f209bae2fcd74d7
Erkki Heino <erkki.heino@aalto.fi>
2017-09-22 14:15:18 +0300

    Generate person resources in process.sh

--- process.sh
+++ process.sh
@@ -48,7 +48,34 @@ sed -r 's/^(p:.*) cidoc:P70_documents (<.*>)/\2 cidoc:P70i_is_documented_in \1/'
 # TODO: Link camps
 # TODO: Link places using Arpa-linker
 
-echo "Finishing" &&
+echo "Finishing prisoners" &&
 
 cat data/new/prisoners_plain.ttl data/new/rank_links.ttl data/new/unit_linked_validated.ttl data/new/persons_linked.ttl > data/new/prisoners_full.ttl &&
-rapper -i turtle data/new/prisoners_full.ttl -o turtle > data/new/prisoners.ttl
+rapper -i turtle data/new/prisoners_full.ttl -o turtle > data/new/prisoners.ttl &&
+
+echo "Generating people..." &&
+
+echo "...Updating db with prisoners" &&
+s-put http://localhost:3030/warsa/data http://ldf.fi/warsa/prisoners data/new/prisoners.ttl &&
+
+echo "...Constructing people" &&
+echo 'query=' | cat - sparql/construct_people.sparql | sed 's/&/%26/g' | curl -d @- http://localhost:3030/warsa/sparql -v > data/new/prisoner_people.ttl &&
+
+echo "...Constructing documents links" &&
+echo 'query=' | cat - sparql/construct_documents_links.sparql | sed 's/&/%26/g' | curl -d @- http://localhost:3030/warsa/sparql -v > data/new/prisoner_documents_links.ttl &&
+
+echo "...Updating db with new people" &&
+cat data/new/prisoner_people.ttl data/new/prisoner_documents_links.ttl > prisoners_temp.ttl &&
+s-put http://localhost:3030/warsa/data http://ldf.fi/warsa/prisoner_persons data/new/prisoner_people.ttl &&
+rm prisoners_temp.ttl &&
+
+for construct in births promotions ranks unit_joinings captures disappearances
+do
+    echo "...Constructing $construct" &&
+    echo 'query=' | cat - "sparql/construct_$construct.sparql" | sed 's/&/%26/g' | curl -d @- http://localhost:3030/warsa/sparql -v > "data/new/prisoner_$construct.ttl"
+done &&
+
+echo "...Deleting temp graph" &&
+s-delete http://localhost:3030/warsa/data http://ldf.fi/warsa/prisoner_persons &&
+
+echo "...Finished"
