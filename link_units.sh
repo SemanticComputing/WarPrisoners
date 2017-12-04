153c4a7b5119a9bb0c225c3935b44f7d7445491d
Heino Erkki <erkki.heino@aalto.fi>
2017-12-04 11:09:13 +0200

    Use correct file name in link_units.sh

--- link_units.sh
+++ link_units.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PROG="python3 -m warsa_linkers.units"
-GET_CANDIDATES="$PROG data/new/prisoners.ttl data/new/unit_candidates.ttl http://candidates/unit http://demo.seco.tkk.fi/arpa/warsa_actor_units --prop http://ldf.fi/schema/warsa/prisoners/unit -n -c -r 3 -w 3"
+GET_CANDIDATES="$PROG data/new/prisoners_plain.ttl data/new/unit_candidates.ttl http://candidates/unit http://demo.seco.tkk.fi/arpa/warsa_actor_units --prop http://ldf.fi/schema/warsa/prisoners/unit -n -c -r 3 -w 3"
 JOIN="$PROG join data/new/unit_candidates.ttl data/new/unit_candidates_combined.ttl http://candidates/unit http://localhost:3030/warsa/sparql -n --prop http://candidates/unit"
-CAT_ARGS="data/new/prisoners.ttl data/new/periods.ttl data/new/unit_candidates_combined.ttl"
+CAT_ARGS="data/new/prisoners_plain.ttl data/new/periods.ttl data/new/unit_candidates_combined.ttl"
 DISAMBIGUATE="$PROG disambiguate_validate sparql/units.sparql data/new/unit_all.ttl data/new/unit_linked_validated.ttl http://ldf.fi/schema/warsa/prisoners/warsa_unit http://localhost:3030/warsa/sparql -n --prop http://candidates/unit -r 3 -w 3"
 
 only_disambiguate=false
